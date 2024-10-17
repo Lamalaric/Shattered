081a8a6c8ca9d83b429ae333ebc4eeee60c23f3c
Amalaric <lamalaric93@gmail.com>
2024-10-17 10:57:47 +0200

    Added double jump mechanic to Player

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -9,8 +9,9 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float jumpCd;
     private Rigidbody2D _playerRb;
+    private bool _canJump = true;
     private int _jumpCount = 2;
-    private float _currentJumpCd = 0f;
+    private float _currentJumpCd;
 
 
     //Récupère le RigidBody au chargement du script
@@ -26,8 +27,6 @@ public class PlayerMovements : MonoBehaviour
         Debug.Log(_jumpCount);
         PlayerMove();
         PlayerJump();
-
-        ResetJumpCount();
     }
 
     //Check for specific key press for Left / Right movements
@@ -51,31 +50,41 @@ public class PlayerMovements : MonoBehaviour
     //Check for a Spacebar click to make the player jump
     private void PlayerJump()
     {
-        if (Input.GetKey(KeyCode.Space) && _jumpCount > 0)
+        if (Input.GetKey(KeyCode.Space) && CanJump())
         {
-            _playerRb.velocity = new Vector2(_playerRb.velocity.x, jumpForce/2);
-            _jumpCount--;      //Remove 1 jump count
-            // jumpCd = 
+            StartCoroutine(JumpRoutine());
         }
     }
     //Indicate wether the player can jump or not
     private bool CanJump()
     {
+        Debug.Log("Nb of jumps:"+_jumpCount+",    Cooldown:"+_currentJumpCd);
         //If no more jump left
-        if (_jumpCount < 0) return false;
+        if (_jumpCount <= 0) return false;
         //If the cooldown for jump is not ready
         if (_currentJumpCd < jumpCd) return false;
         
         return true;
     }
-    
-    //Reset the Jump count if the player touches the ground
-    private void ResetJumpCount()
+
+    private IEnumerator JumpRoutine()
     {
-        //If player touches the ground
-        // if (Collision.)
+        //Avoid concurrent routines
+        if(!_canJump) yield break;
+        //Disable jumping
+        _canJump = false;
+
+        //Proceed to the jump
+        _playerRb.velocity = new Vector2(_playerRb.velocity.x, jumpForce/2);
+        _jumpCount--;      //Remove 1 jump count
+
+        //Waits for 3 seconds and then will come back here
+        yield return new WaitForSeconds(jumpCd);
+
+        // after the cooldown allow next jump
+        _canJump = true;
     }
-    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("ground"))
