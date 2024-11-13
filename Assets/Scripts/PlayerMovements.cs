a4adc24a92173abc98ab1bcbc1a90d53517607d0
Amalaric <lamalaric93@gmail.com>
2024-11-13 15:29:18 +0100

    minor changes

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -143,7 +143,6 @@ public class PlayerMovements : MonoBehaviour
             StartCoroutine(DashRoutine());
         }
     }
-    
     private IEnumerator DashRoutine()
     {
         // Commence le dash
